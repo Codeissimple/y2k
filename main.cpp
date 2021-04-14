56ede7cce0435006510c77e7738d4d144d30f07a
Codeissimple <>
2021-04-14 01:28:51 +0000

    Added more code

--- main.cpp
+++ main.cpp
@@ -3,19 +3,29 @@ using namespace std;
 
 int main() {
   int year;
-  int normal_year;
+  int month;
+  int day;
+  int regular_year;
   int leap_year;
+  int i;
 
-  cout << "Enter a year: ";
-  cin >> year;
+  for (i =0; i < 10; i++){
+    cout << "Enter a year: ";
+    cin >> year;
 
-  if (year % 4 == 0 && (year % 100 != 0) && (year % 460 ==0)){
-    normal_year = year;
-    cout << "The year " << year << " is a regular year.";
+    cout <<"Enter a month: ";
+    cin >> month;
+
+    cout << "Enter a day: ";
+    cin >> day;
+
+    if ( (year % 460 ==0) || ((year % 4 == 0) && (year % 100 != 0))){
+
+      cout << "The year " << year << " is a leap year." << endl;
+      }
+    else{
+      cout << "The year " << year << " is a regular year." << endl;
     }
-  else{
-    leap_year = year;
-    cout << "The year " << leap_year << " is a regular year.";
   }
 return 0;
 }
