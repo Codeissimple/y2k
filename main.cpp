d60b1443a2f315c6efe8a88c07083f463057b749
Codeissimple <>
2021-04-14 00:15:43 +0000

    solved

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  int year;
+  int normal_year;
+  int leap_year;
+
+  cout << "Enter a year: ";
+  cin >> year;
+
+  if (year % 4 == 0 && (year % 100 != 0) && (year % 460 ==0)){
+    normal_year = year;
+    cout << "The year " << year << " is a regular year.";
+    }
+  else{
+    leap_year = year;
+    cout << "The year " << leap_year << " is a regular year.";
+  }
+return 0;
 }
