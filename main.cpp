76be406f6a217183b2310aa865a5feca45f1161c
Codeissimple <>
2021-04-16 02:28:28 +0000

    2

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ bool return_value = false;
   else{
       return_value = false;
     }
+    return 0;
 }
 
 bool is_month_valid(long month){
@@ -19,6 +20,7 @@ bool return_value = false;
   else{
     return_value = false;
   }
+  return 0;
 }
 
 
@@ -40,6 +42,7 @@ long year = 0;
   year =  query_for_integer("Enter a year: ");
   month = query_for_integer("Enter a month: ");
   day = query_for_integer("Enter a day: ");
+
   if (is_a_leap_year(year)){
     cout << "Year is a leap year\n";
   }
@@ -49,12 +52,25 @@ long year = 0;
 
 
   if (is_month_valid(month)){
-    cout << "Month okay.\n";
+    if (month == 2){
+        if (is_a_leap_year(year) == true){
+          cout << "29 days\n";
+          }
+        else{
+          cout << "28 days\n";
+        }
+      }
+    else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+      cout << "31 days\n";
+    }
+    else{
+      cout << "30 days\n";
+    }
   }
   else{
     cout << "Month not okay.\n";
+      return 0;
   }
-
-
-  return 0;
 }
+
+
