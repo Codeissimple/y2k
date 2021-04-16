6a02a4635060189e6fcc0c77f4cd89577550bdee
Codeissimple <>
2021-04-16 01:06:43 +0000

    1

--- main.cpp
+++ main.cpp
@@ -1,31 +1,60 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int year;
-  int month;
-  int day;
-  int regular_year;
-  int leap_year;
-  int i;
-
-  for (i =0; i < 10; i++){
-    cout << "Enter a year: ";
-    cin >> year;
-
-    cout <<"Enter a month: ";
-    cin >> month;
-
-    cout << "Enter a day: ";
-    cin >> day;
-
-    if ( (year % 460 ==0) || ((year % 4 == 0) && (year % 100 != 0))){
-
-      cout << "The year " << year << " is a leap year." << endl;
+bool is_a_leap_year(long year) {
+bool return_value = false;
+  if ( (year % 400 ==0) || ((year % 4 == 0) && (year % 100 != 0))){
+      return_value = true;
       }
-    else{
-      cout << "The year " << year << " is a regular year." << endl;
+  else{
+      return_value = false;
     }
+}
+
+bool is_month_valid(long month){
+bool return_value = false;
+  if (month >= 1 && month <= 12){
+    return_value = true;
   }
-return 0;
+  else{
+    return_value = false;
+  }
+}
+
+
+long query_for_integer(string prompt) {
+long answer = 0;
+
+  cout << prompt;
+  cin >> answer;
+
+  return 0;
+}
+
+
+int main() {
+long day = 0;
+long month = 0;
+long year = 0;
+
+  year =  query_for_integer("Enter a year: ");
+  month = query_for_integer("Enter a month: ");
+  day = query_for_integer("Enter a day: ");
+  if (is_a_leap_year(year)){
+    cout << "Year is a leap year\n";
+  }
+  else{
+    cout << "Year is not a leap year\n";
+  }
+
+
+  if (is_month_valid(month)){
+    cout << "Month okay.\n";
+  }
+  else{
+    cout << "Month not okay.\n";
+  }
+
+
+  return 0;
 }
